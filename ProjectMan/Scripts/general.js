4ffbe04289039e99d75c9476ff30840399598554
Enes Ayyıldız <enesayyildiz0@gmail.com>
2016-05-07 15:53:52 +0300

    datatable & datepicekr init

--- ProjectMan/Scripts/general.js
+++ ProjectMan/Scripts/general.js
@@ -2,8 +2,42 @@
 
 function init() {
     renderDateTables();
+    renderDatePickers();
 }
 
 function renderDateTables() {
-    $(".datatable").DataTable();
+    $(".datatable").DataTable({
+        language: {
+            "sDecimal": ",",
+            "sEmptyTable": "Tabloda herhangi bir veri mevcut değil",
+            "sInfo": "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
+            "sInfoEmpty": "Kayıt yok",
+            "sInfoFiltered": "(_MAX_ kayıt içerisinden bulunan)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Sayfada _MENU_ kayıt göster",
+            "sLoadingRecords": "Yükleniyor...",
+            "sProcessing": "İşleniyor...",
+            "sSearch": "Ara:",
+            "sZeroRecords": "Eşleşen kayıt bulunamadı",
+            "oPaginate": {
+                "sFirst": "İlk",
+                "sLast": "Son",
+                "sNext": "Sonraki",
+                "sPrevious": "Önceki"
+            },
+            "oAria": {
+                "sSortAscending": ": artan sütun sıralamasını aktifleştir",
+                "sSortDescending": ": azalan sütun soralamasını aktifleştir"
+            }
+        }
+    });
+}
+
+function renderDatePickers() {
+    $('[data-type="date"]').datepicker({
+        format: "dd/mm/yyyy",
+        language: "tr",
+        todayHighlight: true
+    });
 }
