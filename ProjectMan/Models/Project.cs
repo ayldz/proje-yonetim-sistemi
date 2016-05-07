57cd75ce3dd1411d606afd9aa424c9965d360e82
alperenayyildiz <alperenayyildiz@outlook.com>
2016-05-07 19:34:52 +0300

    r

--- ProjectMan/Models/Project.cs
+++ ProjectMan/Models/Project.cs
@@ -9,8 +9,6 @@ namespace ProjectMan.Models
     [Table("Project")]
     public partial class Project
     {
-        internal DateTime startDateActual;
-
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public Project()
         {
